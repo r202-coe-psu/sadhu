326a196c322d34735def2f59d21cebfbe8d2067f
Thanathip Limna <boatkrap@gmail.com>
2018-12-24 00:12:23 +0700

    divide dashboard according to user roles

--- sadhu/views/dashboard.py
+++ sadhu/views/dashboard.py
@@ -1,10 +1,20 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 module = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 subviews = []
 
+def index_admin():
+    return render_template('/dashboard/index-admin.html')
+
+def index_user():
+    return render_template('/dashboard/index.html')
+
 @module.route('/')
 @login_required
 def index():
-    return render_template('/dashboard/index.html')
+    user = current_user._get_current_object()
+    if 'lecturer' in user.roles:
+        return index_admin()
+    
+    return index_user()
