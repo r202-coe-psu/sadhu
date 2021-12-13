194bbef9ac308d5bd33b0d1694de83b05934d92b
Thanathip Limna <boatkrap@gmail.com>
2021-12-13 19:07:56 +0700

    fix error

--- sadhu/models/users.py
+++ sadhu/models/users.py
@@ -5,6 +5,7 @@ from flask_login import UserMixin
 
 from .classes import Enrollment
 
+
 class User(me.Document, UserMixin):
     username = me.StringField(required=True, unique=True)
 
@@ -12,19 +13,18 @@ class User(me.Document, UserMixin):
     first_name = me.StringField(required=True)
     last_name = me.StringField(required=True)
 
-    status = me.StringField(required=True, default='disactive')
-    roles = me.ListField(me.StringField(), default=['user'])
+    status = me.StringField(required=True, default="disactive")
+    roles = me.ListField(me.StringField(), default=["user"])
 
-    created_date = me.DateTimeField(required=True,
-                                    default=datetime.datetime.now)
-    updated_date = me.DateTimeField(required=True,
-                                    default=datetime.datetime.now,
-                                    auto_now=True)
+    created_date = me.DateTimeField(required=True, default=datetime.datetime.now)
+    updated_date = me.DateTimeField(
+        required=True, default=datetime.datetime.now, auto_now=True
+    )
 
     resources = me.DictField()
     metadata = me.DictField()
 
-    meta = {'collection': 'users'}
+    meta = {"collection": "users"}
 
     def has_roles(self, roles):
         for role in roles:
@@ -34,28 +34,24 @@ class User(me.Document, UserMixin):
 
     def get_teaching_assistant_class(self):
         from sadhu import models
-        classes = models.Class.objects(
-                teaching_assistants__user=self)
+
+        classes = models.Class.objects(teaching_assistants__user=self)
 
         return classes
 
     def get_image(self):
-        if 'google' in self.resources:
-            return self.resources['google'].get('picture', None)
+        if "google" in self.resources:
+            return self.resources["google"].get("picture", None)
         return None
 
     def get_enrollments(self):
         return Enrollment.objects(user=self)
 
     def enroll(self, class_):
-        enrollment = Enrollment.objects(
-                user=self,
-                enrolled_class=class_).first()
+        enrollment = Enrollment.objects(user=self, enrolled_class=class_).first()
 
         if not enrollment:
-            enrollment = Enrollment(
-                    user=self,
-                    enrolled_class=class_)
+            enrollment = Enrollment(user=self, enrolled_class=class_)
             enrollment.save()
 
         if enrollment not in class_.enrollments:
--- sadhu/web/forms/assignments.py
+++ sadhu/web/forms/assignments.py
@@ -9,36 +9,45 @@ from .fields import TagListField
 
 from flask_wtf import FlaskForm
 
-class AssignmentTimeForm(FlaskForm):
-    started_date = fields.DateTimeField('Started Date',
-            format='%Y-%m-%d %H:%M',
-            default=datetime.date.today,
-            widget=widgets.TextInput(),
-            )
 
-    ended_date = fields.DateTimeField('Ended Data',
-            format='%Y-%m-%d %H:%M',
-            default=default=datetime.date.today,
-            widget=widgets.TextInput(), 
-            )
+class AssignmentTimeForm(FlaskForm):
+    started_date = fields.DateTimeField(
+        "Started Date",
+        format="%Y-%m-%d %H:%M",
+        default=datetime.date.today,
+        widget=widgets.TextInput(),
+    )
+
+    ended_date = fields.DateTimeField(
+        "Ended Data",
+        format="%Y-%m-%d %H:%M",
+        default=datetime.date.today,
+        widget=widgets.TextInput(),
+    )
 
 
 class ChallengeAddingForm(FlaskForm):
-    challenges = fields.SelectMultipleField('Challenges')
+    challenges = fields.SelectMultipleField("Challenges")
+
 
 class AssignmentForm(FlaskForm):
-    name = fields.StringField('Name',
-            validators=[validators.InputRequired(),
-                        validators.Length(min=3)])
-    description = fields.StringField('Description',
-            validators=[validators.InputRequired()],
-            widget=widgets.TextArea())
-    score = fields.IntegerField('Score',
-            validators=[validators.InputRequired(),
-                validators.NumberRange(min=0)],
-            default=0)
-    course = fields.SelectField('Course',)
-#            validators=[validators.InputRequired()])
-    tags = TagListField('Tags',
-            validators=[validators.InputRequired(),
-                        validators.Length(min=3)])
+    name = fields.StringField(
+        "Name", validators=[validators.InputRequired(), validators.Length(min=3)]
+    )
+    description = fields.StringField(
+        "Description",
+        validators=[validators.InputRequired()],
+        widget=widgets.TextArea(),
+    )
+    score = fields.IntegerField(
+        "Score",
+        validators=[validators.InputRequired(), validators.NumberRange(min=0)],
+        default=0,
+    )
+    course = fields.SelectField(
+        "Course",
+    )
+    #            validators=[validators.InputRequired()])
+    tags = TagListField(
+        "Tags", validators=[validators.InputRequired(), validators.Length(min=3)]
+    )
