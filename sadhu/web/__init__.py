14766b18f61423e1724a1ec74675275253078910
Thanathip Limna <boatkrap@gmail.com>
2024-02-27 22:47:51 +0700

    fix error

--- sadhu/web/__init__.py
+++ sadhu/web/__init__.py
@@ -1,4 +1,3 @@
-
 import optparse
 
 from flask import Flask
@@ -11,48 +10,54 @@ from . import oauth2
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('sadhu.default_settings')
-    app.config.from_envvar('SADHU_SETTINGS', silent=True)
+    app.config.from_object("sadhu.default_settings")
+    app.config.from_envvar("SADHU_SETTINGS", silent=True)
 
     models.init_db(app)
     acl.init_acl(app)
     oauth2.init_oauth(app)
-    
+
     views.register_blueprint(app)
 
     return app
 
 
-def get_program_options(default_host='127.0.0.1',
-                        default_port='8080'):
-
+def get_program_options(default_host="127.0.0.1", default_port="8080"):
     """
-    Takes a flask.Flask instance and runs it. Parses 
+    Takes a flask.Flask instance and runs it. Parses
     command-line flags to configure the app.
     """
 
     # Set up the command-line options
     parser = optparse.OptionParser()
-    parser.add_option("-H", "--host",
-                      help="Hostname of the Flask app " + \
-                           "[default %s]" % default_host,
-                      default=default_host)
-    parser.add_option("-P", "--port",
-                      help="Port for the Flask app " + \
-                           "[default %s]" % default_port,
-                      default=default_port)
+    parser.add_option(
+        "-H",
+        "--host",
+        help="Hostname of the Flask app " + "[default %s]" % default_host,
+        default=default_host,
+    )
+    parser.add_option(
+        "-P",
+        "--port",
+        help="Port for the Flask app " + "[default %s]" % default_port,
+        default=default_port,
+    )
 
-    # Two options useful for debugging purposes, but 
+    # Two options useful for debugging purposes, but
     # a bit dangerous so not exposed in the help message.
-    parser.add_option("-c", "--config",
-                      dest="config",
-                      help=optparse.SUPPRESS_HELP, default=None)
-    parser.add_option("-d", "--debug",
-                      action="store_true", dest="debug",
-                      help=optparse.SUPPRESS_HELP)
-    parser.add_option("-p", "--profile",
-                      action="store_true", dest="profile",
-                      help=optparse.SUPPRESS_HELP)
+    parser.add_option(
+        "-c", "--config", dest="config", help=optparse.SUPPRESS_HELP, default=None
+    )
+    parser.add_option(
+        "-d", "--debug", action="store_true", dest="debug", help=optparse.SUPPRESS_HELP
+    )
+    parser.add_option(
+        "-p",
+        "--profile",
+        action="store_true",
+        dest="profile",
+        help=optparse.SUPPRESS_HELP,
+    )
 
     options, _ = parser.parse_args()
 
@@ -61,10 +66,8 @@ def get_program_options(default_host='127.0.0.1',
     if options.profile:
         from werkzeug.contrib.profiler import ProfilerMiddleware
 
-        app.config['PROFILE'] = True
-        app.wsgi_app = ProfilerMiddleware(app.wsgi_app,
-                                          restrictions=[30])
+        app.config["PROFILE"] = True
+        app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
         options.debug = True
 
     return options
-
