da16ba8bc1e5a469c7f60d9aba4be0a2cf0c7907
Thanathip Limna <boatkrap@gmail.com>
2026-06-28 16:20:19 +0700

    fix for multiline student

--- sadhu/web/forms/fields.py
+++ sadhu/web/forms/fields.py
@@ -10,15 +10,16 @@ class TagListField(Field):
         self.remove_duplicates = remove_duplicates
 
     def process_formdata(self, valuelist):
+        self.data = []
         if valuelist:
-            self.data = [
-                x.strip() for x in valuelist[0].split(",") if len(x.strip()) > 0
-            ]
-        else:
-            self.data = []
+            for l in valuelist[0].splitlines():
+                for tag in l.split(","):
+                    tag = tag.strip()
+                    if not tag:
+                        continue
+                    if tag not in self.data:
+                        self.data.append(tag)
 
-        if self.remove_duplicates:
-            self.data = list(self._remove_duplicates(self.data))
 
     def _value(self):
         if self.data:
